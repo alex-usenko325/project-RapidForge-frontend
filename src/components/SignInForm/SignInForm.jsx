35003a5387cdeb7257dec44df8a753927e7e65a2
unknown <andrii.hushuvatyi@gmail.com>
2025-03-07 16:52:46 +0200

    validation signup-signin data

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,15 +1,44 @@
 import clsx from 'clsx';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import Logo from '../Logo/Logo';
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { signin } from '../../redux/auth/operations';
+
+const SingInValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Please enter a valid email')
+    .required('Email is required field!'),
+  password: Yup.string()
+    .min(6, 'Password is too short!')
+    .max(18, 'Password is too long!')
+    .required('Password is required field!'),
+});
+
+const initialValues = {
+  email: '',
+  password: '',
+};
 
 const SignInForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    dispatch(signin(values));
+    actions.resetForm();
+  };
+
   return (
     <div className={clsx('container', 'authContainer')}>
       <div className="authSection">
         <Logo />
         <div className="authWrap">
           <h2 className="authSubtitle">Sign In</h2>
-          <Formik>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={SingInValidationSchema}
+          >
             <Form className="authForm">
               <div className="authFormWrap">
                 <label className="authLabel">
