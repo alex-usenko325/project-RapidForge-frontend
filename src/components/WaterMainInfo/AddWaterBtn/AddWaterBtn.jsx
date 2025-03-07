430616b739ff722ccd873cc0caf13d00eb41107c
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2025-03-07 02:41:05 +0200

    mobil_first_wait_redus

--- src/components/WaterMainInfo/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/WaterMainInfo/AddWaterBtn/AddWaterBtn.jsx
@@ -1,7 +1,29 @@
+import css from './AddWaterBtn.module.css';
+import { useState } from 'react';
+import WaterModal from '../../WaterModal/WaterModal';
+
 export default function AddWaterBtn() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <p>AddWaterBtn</p>
-    </div>
+    // <>
+    //   <button className={css.button}>
+    //     <span className={css.plus}>+</span> Add water
+    //   </button>
+    // </>
+    <>
+      <button className={css.button} onClick={handleClick}>
+        <span className={css.plus}>+</span> Add water
+      </button>
+      {isModalOpen && <WaterModal onClose={handleCloseModal} />}
+    </>
   );
 }
--- src/components/WaterMainInfo/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterMainInfo/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,7 +1,10 @@
+import css from './WaterDailyNorma.module.css';
+
 export default function WaterDailyNorma() {
   return (
-    <div>
-      <p>WaterDailyNorma</p>
+    <div className={css.container}>
+      <p className={css.litr}>1.5 L</p>
+      <p className={css.norma}>My daily norma</p>
     </div>
   );
 }
