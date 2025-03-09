48a2178e203ea3552d4c4afef43f84e4a1f7145d
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-03-09 19:44:55 +0100

    added additional styles

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,13 +1,19 @@
 import s from './DeleteWaterModal.module.css';
+import sprite from '../../assets/sprite.svg';
 
 const DeleteWaterModal = () => {
   return (
     <div className={s.overlay}>
       <div className={s.modal}>
-        <h3 className={s.header}>Delete entry</h3>
-        <p className={s.text}>Are you sure you want to delete the entry?</p>
-        <button className={s.btn_delete}>Delete</button>
-        <button className={s.btn_cancel}>Cancel</button>
+        <use xlinkHref={`${sprite}#icon-close`} />
+        <div className={s.header_text}>
+          <h3 className={s.header}>Delete entry</h3>
+          <p className={s.text}>Are you sure you want to delete the entry?</p>
+        </div>
+        <div className={s.buttons}>
+          <button className={s.btn_delete}>Delete</button>
+          <button className={s.btn_cancel}>Cancel</button>
+        </div>
       </div>
     </div>
   );
