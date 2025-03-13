9625134441d27e06e9e0fe2e54415314035c8ad4
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-03-13 21:07:01 +0100

    fixed userpopover

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,23 +1,12 @@
-import { useState } from 'react';
 import s from './UserSettingsModal.module.css';
 import UserSettingsForm from '../UserSettingsForm/UserSettingsForm.jsx';
 import Modal from '../Modal/Modal.jsx';
 
-export default function UserSettingsModal() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
+export default function UserSettingsModal({ onClose }) {
   return (
-    isOpen && (
-      <div className={s.settings_modal}>
-        <Modal onClose={closeModal} modalClass={s.settings}>
-          <h2>Settings</h2>
-          <UserSettingsForm closeModal={closeModal} />
-        </Modal>
-      </div>
-    )
+    <Modal onClose={onClose} modalClass={s.settings}>
+      <h2>Settings</h2>
+      <UserSettingsForm closeModal={onClose} />
+    </Modal>
   );
 }
