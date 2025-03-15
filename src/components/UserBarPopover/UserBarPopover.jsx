ea66532d2d0e956b1af0a74f1053b88466294f89
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-03-15 17:08:25 +0100

    fixed Userpopover, deleted Layout, fixed UserPanel

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,17 +1,18 @@
 import s from './UserBarPopover.module.css';
+import sprite from '../../assets/sprite.svg';
 
-const UserBarPopover = ({ onClose, openUserSettings, openLogout }) => {
+const UserBarPopover = ({ onClose, openUserSettings, openLogout, width }) => {
   return (
-    <div className={s.userBar}>
+    <div className={s.userBar} style={{ width: `${width}px` }}>
       <button className={s.btn_settings} onClick={openUserSettings}>
         <svg className={s.icon_settings} width="16" height="16">
-          <use href="./sprite.svg#icon-settings"></use>
+          <use xlinkHref={`${sprite}#icon-log-out`} />
         </svg>
-        Setting
+        Settings
       </button>
       <button className={s.btn_logout} onClick={openLogout}>
         <svg className={s.icon_logout} width="16" height="16">
-          <use href="./sprite.svg#icon-log-out"></use>
+          <use xlinkHref={`${sprite}#icon-settings`} />
         </svg>
         Log out
       </button>
