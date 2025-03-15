95b0b7729bd9e54dfc9a9b72379c3e80315a917e
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-03-16 00:30:19 +0100

    corrected usersettings styles

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -9,13 +9,13 @@ const UserBarPopover = ({ onClose, openUserSettings, openLogout, width }) => {
     <div className={s.userBar} style={{ width: `${width}px` }}>
       <button className={s.btn_settings} onClick={openUserSettings}>
         <svg className={s.icon_settings} width="16" height="16">
-          <use xlinkHref={`${sprite}#icon-log-out`} />
+          <use xlinkHref={`${sprite}#icon-settings`} />
         </svg>
         {t('userBarPopover.settings')}
       </button>
       <button className={s.btn_logout} onClick={openLogout}>
         <svg className={s.icon_logout} width="16" height="16">
-          <use xlinkHref={`${sprite}#icon-settings`} />
+          <use xlinkHref={`${sprite}#icon-log-out`} />
         </svg>
         {t('userBarPopover.logout')}
       </button>
