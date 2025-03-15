619e96b6258287d712501d23414aa95cebc0796f
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-03-15 15:50:00 +0200

    почистив від комітів

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -90,63 +90,6 @@ export const deleteWaterRecord = createAsyncThunk(
   }
 );
 
-// export const fetchWaterItem = createAsyncThunk(
-//   'fetchWaterItem',
-//   async (date, thunkApi) => {
-//     try {
-//       const { data } = await waterAPI.get(`/water/day/${date}`);
-//       return data.data.waterDayByHour;
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const addWaterItem = createAsyncThunk(
-//   'addWaterItem',
-//   async (item, thunkApi) => {
-//     try {
-//       const { data } = await waterAPI.post('/water', item);
-//       return data.data;
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const updateWaterItem = createAsyncThunk(
-//   'updateWaterItem',
-//   async (item, thunkApi) => {
-//     try {
-//       const { _id, ...bodyWithoutId } = item;
-//       const { data } = await waterAPI.patch(`water/${_id}`, bodyWithoutId);
-//       return data.data;
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const deleteWaterItem = createAsyncThunk(
-//   'deleteWaterItem',
-//   async (_id, thunkApi) => {
-//     try {
-//       if (!_id || typeof _id !== 'string') {
-//         throw new Error('Invalid ID: ID is missing or not a valid string.');
-//       }
-//       const { data } = await waterAPI.delete(`/water/${_id}`);
-//       console.log('Delete response:', data);
-//       return _id;
-//     } catch (error) {
-//       console.error(
-//         'Error deleting water item:',
-//         error.response?.data || error.message
-//       );
-//       return thunkApi.rejectWithValue(error.response?.data || error.message);
-//     }
-//   }
-// );
-
 export const fetchWaterPer = createAsyncThunk(
   'fetchWaterPer',
   async (date, thunkApi) => {
