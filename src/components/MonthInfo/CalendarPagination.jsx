b86f5d20ef5b786c12f5ab34b1d2060271d6509d
Yuli <yu.expert@gmail.com>
2025-03-16 01:52:44 +0200

    Add transition? fix locale month

--- src/components/MonthInfo/CalendarPagination.jsx
+++ src/components/MonthInfo/CalendarPagination.jsx
@@ -3,8 +3,9 @@ import s from './CalendarPagination.module.css';
 import sprite from '../../assets/sprite.svg';
 import { useTranslation } from 'react-i18next';
 import 'dayjs/locale/uk';
+
 const CalendarPagination = ({ selectedDate, onDateChange }) => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   dayjs.locale('uk');
   const handlePreviousMonth = () => {
     onDateChange(dayjs(selectedDate).subtract(1, 'month').toDate());
@@ -25,7 +26,7 @@ const CalendarPagination = ({ selectedDate, onDateChange }) => {
         </svg>
 
         <span className={s.spanmonth}>
-          {dayjs(selectedDate).format('MMMM, YYYY')}
+          {dayjs(selectedDate).locale(i18n.language).format('MMMM, YYYY')}
         </span>
 
         <svg className={s.btnpagination} onClick={handleNextMonth}>
