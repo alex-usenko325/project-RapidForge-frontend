9bd0fa0c62827d077ba05ca6002419c37d809609
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-03-16 14:38:50 +0200

    fix margin

--- src/components/WaterStatistics/WaterStatistics.jsx
+++ src/components/WaterStatistics/WaterStatistics.jsx
@@ -90,7 +90,7 @@ const WaterStatistic = () => {
     <ResponsiveContainer width="100%" height={300}>
       <AreaChart
         data={data}
-        margin={{ top: 10, right: 30, left: 40, bottom: 0 }}
+        margin={{ top: 10, right: 20, left: 0, bottom: 0 }}
       >
         <defs>
           <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
