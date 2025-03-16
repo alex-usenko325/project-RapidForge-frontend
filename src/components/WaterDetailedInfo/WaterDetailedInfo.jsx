24f313d3ddd91d7db5ea9b59288b30457fe6c790
Sergey-2410 <167428697+Sergey-2410@users.noreply.github.com>
2025-03-16 12:18:32 +0200

    fix

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,14 +1,14 @@
 import UserPanel from '../UserPanel/UserPanel.jsx';
 import DailyInfo from '../DailyInfo/DailyInfo.jsx';
+import MonthInfo from '../MonthInfo/MonthInfo.jsx';
 import styles from './WaterDetailedInfo.module.css';
-import WaterStatistic from '../WaterStatistics/WaterStatistics.jsx';
 
 const WaterDetailedInfo = () => {
   return (
     <div className={styles.wrapper}>
       <UserPanel />
       <DailyInfo />
-      <WaterStatistic />
+      <MonthInfo />
     </div>
   );
 };
