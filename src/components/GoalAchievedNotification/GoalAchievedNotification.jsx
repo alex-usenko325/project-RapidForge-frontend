fd10128be8ca6992d1c02cd76db2f5ca19d4dd2a
Yuli <yu.expert@gmail.com>
2025-03-21 15:06:54 +0200

    Add GoalAchievedNotification component

---
+++ src/components/GoalAchievedNotification/GoalAchievedNotification.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
+
+const GoalAchievedNotification = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  // Для отримання актуальних розмірів вікна
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <>
+      <Confetti
+        width={windowWidth}
+        height={windowHeight}
+        recycle={false}
+        // run={false}
+        // currentTime={2}
+      />
+    </>
+  );
+};
+
+export default GoalAchievedNotification;
