7ee93a5a60094fbadb49f7530d8e57e7cbaafb26
Yuli <yu.expert@gmail.com>
2025-03-21 19:09:22 +0200

    Edit styles of Confetti

--- src/components/GoalAchievedNotification/GoalAchievedNotification.jsx
+++ src/components/GoalAchievedNotification/GoalAchievedNotification.jsx
@@ -4,24 +4,33 @@ import Confetti from 'react-confetti';
 const GoalAchievedNotification = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [documentHeight, setDocumentHeight] = useState(
+    document.documentElement.scrollHeight
+  );
 
   // Для отримання актуальних розмірів вікна
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
       setWindowHeight(window.innerHeight);
+      setDocumentHeight(document.documentElement.scrollHeight); // Оновлення висоти документа
     };
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  // Повертаємо Confetti лише для висоти, більші за певний поріг, щоб він не обривався
+  const confettiHeight =
+    windowHeight > documentHeight ? documentHeight : windowHeight;
+
   return (
     <>
       <Confetti
         width={windowWidth}
-        height={windowHeight}
+        height={confettiHeight} // Використовуємо висоту документа, якщо вона більша
         recycle={false}
         numberOfPieces={750}
+        style={{ position: 'absolute', top: 0, left: 0, zIndex: 999 }}
       />
     </>
   );
