674b62ffae63bada309e50a897096c488c2d7e95
Kateryna <katerynawalls@proton.me>
2025-03-30 21:41:05 -0500

    fix translation

--- src/i18next.js
+++ src/i18next.js
@@ -1,24 +1,28 @@
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import enTranslation from './locales/en/translation.json';  // для англійської
-import uaTranslation from './locales/ua/translation.json'; 
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import enTranslation from './locales/en/translation.json'; // для англійської
+import uaTranslation from './locales/ua/translation.json';
 i18next
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(Backend)
   .init({
     resources: {
-        en: { translation: enTranslation },
-        ua: { translation: uaTranslation },
+      en: { translation: enTranslation },
+      ua: { translation: uaTranslation },
     },
-    lng: "en",
-    fallbackLng: "en", 
+    // lng: "en",
+    fallbackLng: 'en',
     debug: false,
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
     interpolation: {
-        escapeValue: false,
-    }
+      escapeValue: false,
+    },
   });
 
-  export default i18next;
+export default i18next;
